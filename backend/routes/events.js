5a1d594367baf1274007c84ae763b8a4e85a9c6b
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-12-09 14:46:11 +0600

    Роуты для юзеров и событий

---
+++ backend/routes/events.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const Event = require('../models/Event');
+const auth = require('../auth');
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const events = await Event
+            .find({$or: [{sharedUsers: req.user._id}, {user: req.user._id}]})
+            .populate('sharedUsers', 'email -_id');
+        if(events.length === 0) return res.status(404).send({error: 'Событий нет'});
+        return res.send(events);
+    } catch (e) {
+        return res.status(500).send({error: 'Eternal Server Error'});
+    }
+});
+
+router.post('/', auth, async (req, res) => {
+    try {
+        const event = new Event({
+            title: req.body.title,
+            datetime: req.body.datetime,
+            user: req.user._id,
+            sharedUsers: req.body.sharedUsers,
+            duration: req.body.duration
+        });
+        await event.save();
+        return res.send({message: 'Событие добавлено.'});
+    } catch (e) {
+        return res.status(400).send(e);
+    }
+});
+
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const deletedEvent = await Event.findById(req.params.id);
+        if(!deletedEvent) return res.status(404)
+            .send({error: 'Событие не найдено.'});
+        if(!deletedEvent.user !== req.user._id) return res.status(401)
+            .send({error: 'У вас нет прав на удаление этого события'});
+        await Event.deleteOne({_id: deletedEvent._id});
+        return res.send({message: 'Событие удалено.'});
+    } catch (e) {
+        return res.status(500).send(e);
+    }
+});
+
+module.exports = router;
---
+++ backend/routes/users.js
@@ -0,0 +1,81 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const config = require('../config');
+const axios = require('axios');
+const {nanoid} = require('nanoid');
+
+router.post('/', async (req, res) => {
+    try {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email
+        });
+        user.genToken();
+        await user.save();
+        return res.send({message: 'Регистрация прошла успешно. Вы будете перенаправлены на страницу входа.'});
+    } catch (e) {
+        return res.status(400).send(e);
+    }
+});
+
+router.post('/sessions', async (req, res) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+        if(!user) return res.status(404).send({error: 'Пользователь не найден'});
+        const isMatch = await user.checkPass(req.body.password);
+        if(!isMatch) return res.status(400).send({error: 'Неверный пароль'});
+        user.genToken();
+        await user.save({validateBeforeSave: false});
+        return res.send(user);
+    } catch (e) {
+        return res.status(400).send(e);
+    }
+});
+
+router.delete('/sessions', async (req, res) => {
+    const token = req.get('Authorization');
+    try {
+        const success = {message: 'Success'};
+        if (!token) return res.send(success);
+        const user = await User.findOne({token});
+        if (!user) return res.send(success);
+        user.genToken();
+        await user.save({validateBeforeSave: false});
+        return res.send({message: 'Вы вышли из системы'});
+    } catch (e) {
+        return res.status(500).send(e);
+    }
+});
+
+router.post('/facebookLogin', async (req, res) => {
+    const inputToken = req.body.accessToken;
+    const accessToken = config.facebookAccess + '|' + config.facebookSecret;
+    const debugToken = `https://graph.facebook.com/debug_token?input_token=${inputToken}&access_token=${accessToken}`;
+    try {
+        const response = await axios.get(debugToken);
+        if(response.data.data.error) {
+            return res.status(401).send({error: 'Facebook token incorrect'});
+        }
+        if(req.body.id !== response.data.data.user_id) {
+            return res.status(401).send({error: 'Wrong user ID'});
+        }
+
+        let user = await User.findOne({facebookId: req.body.id});
+        if(!user) {
+            user = new User({
+                username: req.body.email,
+                password: nanoid(),
+                facebookId: req.body.id,
+                email: req.body.email
+            });
+        }
+        user.genToken();
+        await user.save({validateBeforeSave: false});
+        return res.send(user);
+    } catch (e) {
+        return res.status(401).send({error: 'Facebook token incorrect'});
+    }
+});
+
+module.exports = router;
