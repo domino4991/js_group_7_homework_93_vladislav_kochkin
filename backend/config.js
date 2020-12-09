2b4643a3a188079c9e8ac90172cec3ac86bc69f7
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-12-09 14:46:24 +0600

    Конфиг и миддлваре

---
+++ backend/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    database: "mongodb://localhost/eventsApp",
+    databaseOpt: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    facebookAccess: '2951393108447332',
+    facebookSecret: '8e9ac4d530ef75f211b0e35fcd8fb549'
+};
