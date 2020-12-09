0420943a17d069b3920466906b102af7a7d022e6
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-12-09 17:42:07 +0600

    Вывод событий

--- frontend/src/containers/Main/Main.js
+++ frontend/src/containers/Main/Main.js
@@ -1,8 +1,17 @@
 import React, {useEffect} from 'react';
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {getEvents} from "../../store/actions/eventsActions";
+import { makeStyles, Typography, List, ListItem, ListItemText, Grid } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+    title: {
+        marginBottom: theme.spacing(4)
+    }
+}));
 
 const Main = () => {
+    const classes = useStyles();
+    const {events, eventsError} = useSelector(state => state.events);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -11,7 +20,28 @@ const Main = () => {
 
     return (
         <div>
-            <h1>Hello</h1>
+            <Typography
+                variant='h5'
+                component='h5'
+                align='center'
+                className={classes.title}
+            >
+                Календарь событий
+            </Typography>
+            <Grid container >
+                <Grid item xs={12}>
+                    <List>
+                        {events && events.map(event => <ListItem
+                            key={event._id}
+                            button
+                        >
+                            <ListItemText>
+                                {event.title}
+                            </ListItemText>
+                        </ListItem>)}
+                    </List>
+                </Grid>
+            </Grid>
         </div>
     );
 };
